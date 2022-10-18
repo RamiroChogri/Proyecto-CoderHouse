bf5646828feda3e0fe4d47c86a07ad964774362b
Quique-Pizza <46098047+Quique-Pizza@users.noreply.github.com>
2022-10-17 23:36:26 -0300

    Revert "Merge branch 'main' of https://github.com/RamiroChogri/Proyecto-CoderHouse"
    This reverts commit 1d6e07aa9061e6f491fe7db689839122c7670b88, reversing
    changes made to 92b0b7fdf5f16d5c5ca385e14792fa62fca6bc90.

--- ProjectoCoderHouse/Assets/C#/Player.cs
+++ ProjectoCoderHouse/Assets/C#/Player.cs
@@ -5,12 +5,7 @@ public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     float m_Thrust = 20f;
-    float m_AxisMultiplier = 2f;
     Rigidbody m_Rigidbody;
-
-    float MAXENERGY = 100f;
-    float energy = 100f;
-    float energyRechargeRate = 20f;
     Vector3 inputForce;
 
     public GameObject camOne;
@@ -26,22 +21,13 @@ public class Player : MonoBehaviour
        ToggleCam(); 
     }
     void FixedUpdate()
-    {   
-        if(energy < MAXENERGY){
-            energy += energyRechargeRate;
-        }
+    {
 
-        if(Input.GetMouseButton(0) && energy > 0f){
-            m_AxisMultiplier = 4f;
-            energy-=10f;
-        }else{
-            m_AxisMultiplier = 2f;
-        }
 
-        inputForce.x = Input.GetAxis("Horizontal")*m_AxisMultiplier;
-        inputForce.z = 1;
-        inputForce *= m_Thrust;
-        m_Rigidbody.AddForce(inputForce * Time.fixedDeltaTime, ForceMode.Impulse);
+        inputForce.x = Input.GetAxis("Horizontal");
+        inputForce.y = Input.GetAxis("Vertical");
+        inputForce.z = 1f;
+        m_Rigidbody.AddForce(inputForce * m_Thrust * Time.fixedDeltaTime, ForceMode.Impulse);
     }
     void ToggleCam()
     {
