de2d89911983351c2d76894f60d0d68d326ec63c
Ramiro Chogri <rchogri@fi.uba.ar>
2022-10-17 23:16:54 -0300

    cambios grales

--- ProjectoCoderHouse/Assets/Player.cs
+++ ProjectoCoderHouse/Assets/Player.cs
@@ -1,10 +1,16 @@
+
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     float m_Thrust = 20f;
+    float m_AxisMultiplier = 2f;
     Rigidbody m_Rigidbody;
+
+    float MAXENERGY = 100f;
+    float energy = 100f;
+    float energyRechargeRate = 20f;
     Vector3 inputForce;
     void Start()
     {
@@ -14,12 +20,21 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate()
-    {
+    {   
+        if(energy < MAXENERGY){
+            energy += energyRechargeRate;
+        }
 
+        if(Input.GetMouseButton(0) && energy > 0f){
+            m_AxisMultiplier = 4f;
+            energy-=10f;
+        }else{
+            m_AxisMultiplier = 2f;
+        }
 
-        inputForce.x = Input.GetAxis("Horizontal");
-        inputForce.y = Input.GetAxis("Vertical");
-        inputForce.z = 20f;
-        m_Rigidbody.AddForce(inputForce * m_Thrust * Time.fixedDeltaTime, ForceMode.Impulse);
+        inputForce.x = Input.GetAxis("Horizontal")*m_AxisMultiplier;
+        //inputForce.z = 1;
+        inputForce *= m_Thrust;
+        m_Rigidbody.AddForce(inputForce * Time.fixedDeltaTime, ForceMode.Impulse);
     }
 }
