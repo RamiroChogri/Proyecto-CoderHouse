68c5cf7cefe0cc0591285631bb72bcfa4c1285a6
Ramiro Chogri <rchogri@fi.uba.ar>
2022-10-17 22:35:19 -0300

    Revamp movimiento

--- ProjectoCoderHouse/Assets/Player.cs
+++ ProjectoCoderHouse/Assets/Player.cs
@@ -1,6 +1,3 @@
-using System.Diagnostics;
-using System.Security.Cryptography;
-using System.Threading;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -8,21 +5,21 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     float m_Thrust = 20f;
     Rigidbody m_Rigidbody;
+    Vector3 inputForce;
     void Start()
     {
+        inputForce = Vector3.zero;
         m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(Input.GetKeyDown(KeyCode.A)){
-            m_Rigidbody.AddForce(Vector3.left * 100f * Time.fixedDeltaTime, ForceMode.Impulse);
-        }
-        if(Input.GetKeyDown(KeyCode.D)){
-            m_Rigidbody.AddForce(Vector3.right * 100f * Time.fixedDeltaTime, ForceMode.Impulse);
-        }
-        m_Rigidbody.AddForce(Vector3.forward * m_Thrust * Time.fixedDeltaTime, ForceMode.Impulse);
 
+
+        inputForce.x = Input.GetAxis("Horizontal");
+        inputForce.y = Input.GetAxis("Vertical");
+        inputForce.z = 20f;
+        m_Rigidbody.AddForce(inputForce * m_Thrust * Time.fixedDeltaTime, ForceMode.Impulse);
     }
 }
