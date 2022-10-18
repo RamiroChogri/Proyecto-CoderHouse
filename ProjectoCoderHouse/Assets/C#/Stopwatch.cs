c8873ad6ca8e60eafb258420cfd394a8fb761eb8
Quique-Pizza <46098047+Quique-Pizza@users.noreply.github.com>
2022-10-17 22:45:32 -0300

    Stopwatch

---
+++ ProjectoCoderHouse/Assets/C#/Stopwatch.cs
@@ -0,0 +1,39 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+
+public class Stopwatch : MonoBehaviour
+{
+    bool stopwatchActive = false;
+    float currentTime;
+    public Text currentTimeText;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTimeText.enabled = false;
+        currentTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stopwatchActive == true)
+        {
+            currentTime = currentTime + Time.deltaTime;
+        }
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currentTimeText.text = time.ToString(@"mm\:ss\:ff");
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.transform.gameObject.name == "StartLine")
+        {
+            currentTimeText.enabled = true;
+            stopwatchActive = true;
+        }
+    }
+}
