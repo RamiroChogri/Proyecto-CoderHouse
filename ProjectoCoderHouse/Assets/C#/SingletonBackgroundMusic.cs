1dae4dc35914a95153fb861bdd1220d988a8589d
Quique-Pizza <46098047+Quique-Pizza@users.noreply.github.com>
2022-10-24 23:04:54 -0300

    Singleton BackMusic

---
+++ ProjectoCoderHouse/Assets/C#/SingletonBackgroundMusic.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonBackgroundMusic : MonoBehaviour
+{
+    public AudioSource MusicSource;
+    public static SingletonBackgroundMusic Instance = null;
+    private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad (gameObject);
+    }
+    public void PlayMusic(AudioClip clip)
+	{
+		MusicSource.clip = clip;
+		MusicSource.Play();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
